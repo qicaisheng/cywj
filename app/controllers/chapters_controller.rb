2173fc80d27dd66c2900b6b870641783299982b1
qicaisheng <qicaisheng@qq.com>
2015-07-12 23:03:31 +0800

    basic

---
+++ app/controllers/chapters_controller.rb
@@ -0,0 +1,45 @@
+class ChaptersController < ApplicationController
+  def index
+    @chapters = Chapter.all
+  end
+
+  def new
+    @novel = Novel.find(params[:novel_id])
+    @chapter = @novel.chapters.new
+  end
+
+  def create
+    @novel = Novel.find(params[:novel_id])
+    @chapter = @novel.chapters.build(chapter_params)
+    @chapter.novel_id = @novel.id
+    @chapter.save
+    redirect_to current_author_path
+  end
+
+  def edit
+    @novel = Novel.find(params[:novel_id])
+    @chapter = @novel.chapters.find(params[:id])
+  end
+
+  def update
+    @novel = Novel.find(params[:novel_id])
+    @chapter = @novel.chapters.find(params[:id])
+    if @chapter.update(chapter_params)
+      flash[:success] = "您已成功更新章节资料"
+      redirect_to current_author_path
+    else
+      flash[:error] = "更新失败"
+      render 'edit'
+    end
+  end
+
+  def show
+    @novel = Novel.find(params[:novel_id])
+    @chapter = @novel.chapters.find(params[:id])
+  end
+
+  private
+    def chapter_params
+      params.require(:chapter).permit(:number, :name, :content, :keywords)
+    end
+end
