a840cbf31094acc00270f38b1853804338e0af34
qicaisheng <qicaisheng@qq.com>
2015-08-02 16:59:01 +0800

    editor upload image add qiniu store and some other needs

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -2,11 +2,18 @@
 class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
   include RedactorRails::Backend::CarrierWave
 
-  # storage :fog
-  storage :file
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
+  # Choose what kind of storage to use for this uploader:
+  storage :qiniu
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "system/redactor_assets/documents/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def extension_white_list
