07e973498c28e21f8e9955ee5245ecbed9fbd696
qicaisheng <qicaisheng@qq.com>
2015-08-09 10:08:39 +0800

    redact rails precomplie

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Cywj
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = 'zh-CN'
+
+    config.assets.precompile += ["redactor-rails/*"]
   end
 end
